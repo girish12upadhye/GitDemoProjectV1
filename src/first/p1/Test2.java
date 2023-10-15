295a0e0f27219d04c74e469da51199c1b700f22b
girish12upadhye <girishupadhye12@gmail.com>
2023-10-15 16:31:32 +0530

    Added additional Checks.......

--- src/first/p1/Test2.java
+++ src/first/p1/Test2.java
@@ -3,8 +3,7 @@ package first.p1;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Lead Asking For changes...");
 	}
 
 }
